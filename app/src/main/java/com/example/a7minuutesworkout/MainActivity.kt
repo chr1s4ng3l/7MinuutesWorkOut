5796363ef4cd364e7f2d00903c40ca0ab36b89fd
Consultant <Consultant@CesarRojas-MacBook-Pro.local>
2023-01-30 09:29:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/a7minuutesworkout/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.a7minuutesworkout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.example.a7minuutesworkout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+
+        binding?.flsStart?.setOnClickListener {
+            Toast.makeText(this, "Start", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
