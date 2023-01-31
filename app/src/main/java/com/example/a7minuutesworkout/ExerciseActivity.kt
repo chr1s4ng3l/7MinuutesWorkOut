24f19149c156bb07e7929402be82fbcd5514bab0
Consultant <christopher.tamayocazarez@gmail.com>
2023-01-30 22:23:49 -0500

    Progress bar

--- app/src/main/java/com/example/a7minuutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minuutesworkout/ExerciseActivity.kt
@@ -2,11 +2,16 @@ package com.example.a7minuutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.a7minuutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     private var binding: ActivityExerciseBinding? = null
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +20,7 @@ class ExerciseActivity : AppCompatActivity() {
 
         setSupportActionBar(binding?.toolbarExercise)
 
-        if (supportActionBar != null){
+        if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
 
@@ -23,5 +28,42 @@ class ExerciseActivity : AppCompatActivity() {
 
             onBackPressed()
         }
+        setUpRestView()
+    }
+
+    private fun setUpRestView() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+
+    }
+
+    private fun setRestProgressBar() {
+        binding?.progressBar?.progress = restProgress
+
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.textViewTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "This is a test timer", Toast.LENGTH_LONG)
+                    .show()
+            }
+
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        binding = null
     }
 }
--- app/src/main/java/com/example/a7minuutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minuutesworkout/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.a7minuutesworkout
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.widget.Toast
 import com.example.a7minuutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -26,4 +28,5 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         binding = null
     }
+
 }
